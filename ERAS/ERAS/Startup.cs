091b2651ce5a2296f5020adb189b6bdb9c9414ca
Harrison Uhunghama <engrbrain@gmail.com>
2018-06-22 11:11:49 +0100

    Project Initialization

---
+++ ERAS/ERAS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ERAS.Startup))]
+namespace ERAS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
